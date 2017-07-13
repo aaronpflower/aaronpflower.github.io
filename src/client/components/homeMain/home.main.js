32425d13f9c5058e6f97ea3251e681e42253899a
Aaron Flower <aaronpflower@gmail.com>
2017-07-12 21:33:40 -0600

    landing page style tweaks

--- src/client/components/homeMain/home.main.js
+++ src/client/components/homeMain/home.main.js
@@ -8,22 +8,9 @@ function HomeMain (props) {
   return (
     <div className={styles.container}>
       <div className={styles.name}>
-        <div className={classnames(fonts.mediumText, styles.firstName)}>
-          <span>A</span>
-          <span>A</span>
-          <span>R</span>
-          <span>O</span>
-          <span>N</span>
-        </div>
-        <div className={classnames(fonts.largeText, styles.lastName)}>
-          <span className={styles.letter}>F</span>
-          <span className={styles.letter}>L</span>
-          <span className={styles.letter}>O</span>
-          <span className={styles.letter}>W</span>
-          <span className={styles.letter}>E</span>
-          <span className={styles.letter}>R</span>
-        </div>
-        <p className={classnames(styles.occupation, styles.spacer, fonts.smallText)}>Javascript Developer</p>
+        <p className={classnames(fonts.mediumText, styles.firstName)}>AARON</p>
+        <p className={classnames(fonts.largeText, styles.lastName)}>FLOWER</p>
+        <p className={classnames(styles.occupation, fonts.mediumTextBold)}>Javascript Developer</p>
         <p className={classnames(styles.spacer, fonts.smallText, styles.strengths)}>Javascript <span className={styles.bar}>|</span> HTML5 <span className={styles.bar}>|</span> CSS3 <span className={styles.bar}>|</span> React <span className={styles.bar}>|</span> Redux <span className={styles.bar}>|</span> Node</p>
         <a href={require("../../base/assets/AaronFlowerResume.pdf")} download="AaronFlowerResume.pdf">
           <button className={classnames(styles.spacer, fonts.smallText, styles.btn)}>Resume</button>
